daff004c2c8ea2a38d4f72adb0b45dd13fc96750
Alexey <fav1290@gmail.com>
2023-12-09 16:16:53 +0300

    Level three completed

--- django_views_routing_homework/views/level_3/b_validate_user_data.py
+++ django_views_routing_homework/views/level_3/b_validate_user_data.py
@@ -18,8 +18,49 @@
 Когда будете писать код, не забывайте о читаемости, поддерживаемости и модульности.
 """
 
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, JsonResponse, HttpResponseBadRequest
+from typing import Mapping
+
+
+def check_data_structure(user_data: Mapping[str, str]) -> bool:
+    check_set = {'full_name', 'email', 'registered_from', 'age'}
+    keys_set = set(user_data.keys())
+    return keys_set <= check_set and check_set - keys_set in [set(), {'age'}]
+
+
+def check_full_name(user_data: Mapping[str, str]) -> bool:
+    user_full_name = user_data.get('full_name', '')
+    return 5 <= len(user_full_name) <= 256
+
+
+def check_email(user_data: Mapping[str, str]) -> bool:
+    user_email = user_data.get('email', '')
+    at_sign_check = user_email.count('@') == 1 and user_email.find('@') >= 0
+    dot_check = user_email.count('.')
+    return all([at_sign_check, dot_check])
+
+
+def check_registered_from(user_data: Mapping[str, str]) -> bool:
+    user_registered_from = user_data.get('registered_from', '')
+    return user_registered_from in ['website', 'mobile_app']
+
+
+def check_age(user_data: Mapping[str, str]) -> bool:
+    user_age = user_data.get('age', None)
+    if user_age is None:
+        return len(user_data) == 3
+    else:
+        return user_age.isdigit()
 
 
 def validate_user_data_view(request: HttpRequest) -> HttpResponse:
-    pass  # код писать тут
+    user_data = request.POST
+    if not check_data_structure(user_data):
+        return HttpResponseBadRequest('Bad data structure found')
+    is_user_data_valid = all([
+        check_full_name(user_data),
+        check_email(user_data),
+        check_registered_from(user_data),
+        check_age(user_data),
+    ])
+    return JsonResponse({'is_valid': is_user_data_valid})
--- django_views_routing_homework/views/level_3/d_file_generation.py
+++ django_views_routing_homework/views/level_3/d_file_generation.py
@@ -13,9 +13,30 @@
 Для проверки используйте браузер: когда ручка правильно работает, при попытке зайти на неё, браузер должен
 скачивать сгенерированный файл.
 """
+import string
+import random
+from faker import Faker
+from django.http import HttpResponse, HttpRequest, HttpResponseForbidden
 
-from django.http import HttpResponse, HttpRequest
+
+def generate_text_handler(length: int) -> str:
+    fake = Faker('la')
+    fake_text: str = fake.text(max_nb_chars=length) if length > 5 else ''
+
+    missing_length = length - len(fake_text)
+    missing_chars = random.choices((string.ascii_lowercase + ' '), k=missing_length)
+    missing_chars[0], missing_chars[-1] = random.choices(string.ascii_lowercase, k=2)
+    fake_text += ''.join(missing_chars)
+
+    return fake_text
 
 
 def generate_file_with_text_view(request: HttpRequest) -> HttpResponse:
-    pass  # код писать тут
+    target_length_str = request.GET.get('length', '0')
+    if target_length_str.isdigit() and 0 < int(target_length_str) <= 1000000:
+        file_content = generate_text_handler(int(target_length_str))
+        response = HttpResponse(file_content, content_type='text/plain')
+        response['Content-Disposition'] = f'attachment; filename={"lorem.txt"}'
+        return response
+    else:
+        return HttpResponseForbidden('length param is incorrect or missing')
